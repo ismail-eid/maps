3fec1bbe1506b4ead0ad8a62189b023f82d28064
Ismail Eid <ismacileid122@gmail.com>
2024-02-01 07:53:46 +0300

    initial commit

---
+++ src/Company.ts
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker';
+
+export  class Company {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number,
+    lng: number
+  };
+
+  constructor () {
+    this.companyName = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude()
+    }
+  }
+
+  markerContent (): string {
+    return `
+    Company Name: ${this.companyName}
+    Catchphrase: ${this.catchPhrase}`
+  }
+}
